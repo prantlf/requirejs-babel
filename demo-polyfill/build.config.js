89e20a9e6c2951e25b6c94cd334200e74eb5a69a
Ferdinand Prantl <prantlf@gmail.com>
2019-04-01 21:23:31 +0200

    Reformat sources to use indentation of 2 spaces
    Introduce eslint for static code checks.

--- demo-polyfill/build.config.js
+++ demo-polyfill/build.config.js
@@ -1,29 +1,29 @@
 ({
-    'baseUrl': './',
+  baseUrl: './',
 
-    'name': 'src/index',
-    'out': 'main-built.js',
+  name: 'src/index',
+  out: 'main-built.js',
 
-    'paths': {
-        'es6': '../es6',
-        'babel': '../node_modules/@babel/standalone/babel.min',
-        'babel-plugin-module-resolver': '../node_modules/babel-plugin-module-resolver-standalone/index'
-    },
+  paths: {
+    es6: '../es6',
+    babel: '../node_modules/@babel/standalone/babel.min',
+    'babel-plugin-module-resolver': '../node_modules/babel-plugin-module-resolver-standalone/index'
+  },
 
-    'config': {
-        'es6': {
-            'extraPlugins': ['transform-async-to-generator', 'external-helpers'],
-            'presets': ['es2015']
-        }
-    },
-
-    'exclude': ['babel', 'babel-plugin-module-resolver'],
-
-    'optimize': 'uglify2',
-    'generateSourceMaps': true,
-    'preserveLicenseComments': false,
-
-    'pragmasOnSave': {
-        'excludeBabel': true
+  config: {
+    es6: {
+      extraPlugins: ['transform-async-to-generator', 'external-helpers'],
+      presets: ['es2015']
     }
+  },
+
+  exclude: ['babel', 'babel-plugin-module-resolver'],
+
+  optimize: 'uglify2',
+  generateSourceMaps: true,
+  preserveLicenseComments: false,
+
+  pragmasOnSave: {
+    excludeBabel: true
+  }
 })
--- demo/build.config.js
+++ demo/build.config.js
@@ -1,20 +1,20 @@
 ({
-    'baseUrl': './',
+  baseUrl: './',
 
-    'name': 'src/index',
-    'out': 'main-built.js',
+  name: 'src/index',
+  out: 'main-built.js',
 
-    'paths': {
-        'es6': '../es6',
-        'babel': '../node_modules/@babel/standalone/babel.min',
-        'babel-plugin-module-resolver': '../node_modules/babel-plugin-module-resolver-standalone/index'
-    },
+  paths: {
+    es6: '../es6',
+    babel: '../node_modules/@babel/standalone/babel.min',
+    'babel-plugin-module-resolver': '../node_modules/babel-plugin-module-resolver-standalone/index'
+  },
 
-    'exclude': ['babel', 'babel-plugin-module-resolver'],
+  exclude: ['babel', 'babel-plugin-module-resolver'],
 
-    'optimize': 'none',
+  optimize: 'none',
 
-    'pragmasOnSave': {
-        'excludeBabel': true
-    }
+  pragmasOnSave: {
+    excludeBabel: true
+  }
 })
