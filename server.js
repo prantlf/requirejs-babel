51c1874c231517e290df0da1734e42f0cdf660d4
Ferdinand Prantl <prantlf@gmail.com>
2019-04-02 03:29:09 +0200

    chore: Log URLs requested from the local web server

--- server.js
+++ server.js
@@ -2,10 +2,31 @@ const connect = require('connect')
 const blockFavicon = require('connect-block-favicon')
 const serveStatic = require('serve-static')
 
+const pad = {
+  twoZeros (number) {
+    return number < 10 ? `0${number}` : number
+  },
+  threeZeros (number) {
+    return number < 10 ? `00${number}` : number < 100 ? `0${number}` : number
+  }
+}
+
+function log (req, res, next) {
+  const date = new Date
+  console.log(`${time('Hour')}:${time('Minute')}:${time('Second')}.${time('Millisecond', 'three')} ${req.url}`)
+  next()
+
+  function time (part, padding = 'two') {
+    const value = date[`get${part}s`]()
+    return pad[`${padding}Zeros`](value)
+  }
+}
+
 function startServer ({ root = '.', port = 8967 } = {}) {
   console.log(`Starting a local web server in the directory "${root}" on the port ${port}...`)
   return new Promise((resolve, reject) => {
     const server = connect()
+      .use(log)
       .use(blockFavicon())
       .use(serveStatic(root, {
         etag: false
